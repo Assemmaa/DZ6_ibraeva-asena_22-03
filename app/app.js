575b685d19df6d8121a86b4204f7958daa433d4b
Assemmaa <asemaibraeva88@gmail.com>
2023-01-24 23:41:43 +0600

    solve homework

--- app/app.js
+++ app/app.js
@@ -1,15 +1,18 @@
-document.querySelector(".btn").addEventListener('click', function (){
+// document.querySelector(".btn").addEventListener('click', function (){
+//     console.log('you click tne button')
+// })
+// document.querySelector(".btn1").addEventListener('click', function (){
+//     console.log('you click tne third button')
+// })
+// document.querySelector(".btn2").addEventListener('click', function (){
+//     console.log('you click tne second button')
+// })
+
+document.querySelectorAll(".btn")
+document.addEventListener('click', function (){
     console.log('you click tne button')
 })
 
-
-
-
-// let button = document.querySelectorAll('.btn');
-
-
-
-// counter
 let btnPlus = document.querySelector(".counter-plus");
 let btnMinus = document.querySelector(".counter-minus");
 let counterText = document.querySelector(".counter");
