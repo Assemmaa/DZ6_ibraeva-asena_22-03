348f06252e84a1ccb45b7975268e8d1ca074578a
Assemmaa <asemaibraeva88@gmail.com>
2023-01-24 23:44:53 +0600

    solve homework

--- app/app.js
+++ app/app.js
@@ -17,11 +17,9 @@ let btnPlus = document.querySelector(".counter-plus");
 let btnMinus = document.querySelector(".counter-minus");
 let counterText = document.querySelector(".counter");
 btnPlus.addEventListener("click", () => {
-    counterText.style.color = "green";
     counterText.textContent++;
 });
 btnMinus.addEventListener("click", () => {
-    counterText.style.color = "red";
     counterText.textContent != 0 ? counterText.textContent-- : false;
 });
 
